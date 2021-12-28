85d83ac3710c3f19da3dedc5dd9f827507499ceb
manchikooo <manchiko.man31@mail.ru>
2021-12-28 17:56:14 +0300

    added in ProfileInfo 'About me: etc'

--- src/components/Navbar/Profile/Profile.tsx
+++ src/components/Navbar/Profile/Profile.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import MyPosts from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 import {PostType} from "./MyPosts/Post/Post";
 import {ActionsType} from "../../../Redux/store";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
 type ProfileType = {
-    allPosts: Array<PostType>
-    newPostText: string
-    dispatch: (action: ActionsType) => void
 }
 
 export const Profile = (props: ProfileType) => {
@@ -16,10 +12,7 @@ export const Profile = (props: ProfileType) => {
     return (
         <div>
             <ProfileInfo/>
-            <MyPostsContainer allPosts={props.allPosts}
-                              newPostText={props.newPostText}
-                              dispatch={props.dispatch}
-            />
+            <MyPostsContainer/>
         </div>
     )
 }
--- src/components/Navbar/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Navbar/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,9 +3,10 @@ import classes from './ProfileInfo.module.css'
 
 export const ProfileInfo = () => {
     return (
-            <div className={classes.descriptionBlock}>
-                <img className={classes.ava}
-                     src='https://icon-library.com/images/avatar-icon/avatar-icon-4.jpg'/>
-            </div>
+        <div className={classes.descriptionBlock}>
+            <img className={classes.ava}
+                 src='https://icon-library.com/images/avatar-icon/avatar-icon-4.jpg'/>
+            <span>About me: etc</span>
+        </div>
     )
 }
